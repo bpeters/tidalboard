793fcaf1b825dd63f0b19e2e595153f71d9fb356
Brennen Peters <brennen.peters@civitaslearning.com>
2015-03-31 01:54:22 -0500

    Work on widget details

--- public/js/browserify/bundle.js
+++ public/js/browserify/bundle.js
@@ -29181,21 +29181,21 @@ module.exports = React.createClass({
 	toggleLeftNav: function() {
 		this.refs.LeftNav.toggle();
 	},
-	toggleAddWidget: function() {
+	addWidget: function() {
 		
 	},
 	render: function() {
 		return (
 			React.createElement("div", null, 
-				React.createElement(AppBar, {title: "Title", onMenuIconButtonTouchTap: this.toggleLeftNav}), 
+				React.createElement(AppBar, {title: "Board Title", onMenuIconButtonTouchTap: this.toggleLeftNav}), 
 				React.createElement(LeftNav, {ref: "LeftNav", docked: false, menuItems: MENU_ITEMS}), 
 				React.createElement(ReactGridLayout, {className: "layout", cols: 12, rowHeight: 30}, 
 					React.createElement(Paper, {innerClassName: "flex-button", zDepth: 1, key: 1, _grid: {static: true, x: 0, y: 0, w: 2, h: 6}}, 
-						React.createElement(FlatButton, {primary: true, onTouchTap: this.toggleAddWidget}, 
+						React.createElement(FlatButton, {primary: true, onTouchTap: this.addWidget}, 
 							React.createElement("i", {className: "fa fa-plus"})
 						)
 					), 
-					React.createElement("div", {key: 2, _grid: {x: 2, y: 0, w: 3, h: 6}}, 
+					React.createElement("div", {key: 2, _grid: {static: true, x: 2, y: 0, w: 3, h: 6}}, 
 						React.createElement(Widget, null)
 					)
 				)
@@ -29210,29 +29210,83 @@ var MUI = require('material-ui')
 var Paper = MUI.Paper;
 var FlatButton = MUI.FlatButton;
 var TextField = MUI.TextField;
+var Toolbar = MUI.Toolbar;
+var ToolbarGroup = MUI.ToolbarGroup;
+var Dialog = MUI.Dialog;
+var IconButton = MUI.IconButton
+
+var LINK_ACTIONS = [
+  { text: 'Cancel' },
+  { text: 'Submit', onClick: this.onLinkSubmit }
+];
 
 module.exports = React.createClass({
 	displayName: 'Widget',
+	getInitialState: function() {
+		return {
+			title: null,
+			descr: null
+		};
+	},
+	handleTitleChange: function(e) {
+		this.setState({
+			title: e.target.value
+		});
+	},
+	handleDescrChange: function(e) {
+		this.setState({
+			descr: e.target.value
+		});
+	},
+	showLinkDialog: function() {
+		this.refs.LinkDialog.show();
+	},
+	onLinkSubmit: function(link) {
+		console.log(link);
+	},
 	render: function() {
 		return (
 			React.createElement(Paper, {innerClassName: "flex-form", zDepth: 1}, 
-				React.createElement(TextField, {hintText: "Title"}), 
-				React.createElement(FlatButton, {label: "Add Title", primary: true}), 
-				React.createElement(TextField, {hintText: "Description", multiLine: true}), 
-				React.createElement(FlatButton, {label: "Add Description", primary: true}), 
-				React.createElement(TextField, {hintText: "Link"}), 
-				React.createElement(FlatButton, {label: "Add Link", primary: true}), 
-				React.createElement(FlatButton, {primary: true}, 
-					React.createElement("i", {className: "fa fa-file-image-o"}), 
-					React.createElement("span", {className: "image-button"}, "Upload Image")
-				), 
-				React.createElement(FlatButton, {primary: true}, 
-					React.createElement("i", {className: "fa fa-file-text-o"}), 
-					React.createElement("span", {className: "image-button"}, "Upload File")
+				this.renderTitle(), 
+				this.renderDescr(), 
+				this.renderButtonBar(), 
+				React.createElement(Dialog, {ref: "LinkDialog", title: "Dialog With Standard Actions", actions: LINK_ACTIONS}, 
+				  "The actions in this window are created from the json that's passed in." 
 				)
 			)
 		);
-	}
-});
+	},
+	renderTitle: function() {
+		return (
+			React.createElement(Toolbar, null, 
+				React.createElement("div", {className: "flex-text"}, 
+					React.createElement(TextField, {hintText: "Title", value: this.state.title, onChange: this.handleTitleChange})
+				)
+			)
+		);
+	},
+	renderDescr: function() {
+		return (
+			React.createElement("div", {className: "flex-text"}, 
+				React.createElement(TextField, {hintText: "Description", multiLine: true, value: this.state.descr, onChange: this.handleDescrChange})
+			)
+		);
+	},
+	renderButtonBar: function() {
+		return (
+			React.createElement("div", {className: "flex-button-bar"}, 
+				React.createElement(IconButton, {tooltip: "Add Link", touch: true}, 
+					React.createElement("i", {className: "fa fa-link"})
+				), 
+				React.createElement(IconButton, {tooltip: "Add Image", touch: true}, 
+					React.createElement("i", {className: "fa fa-file-image-o"})
+				), 
+				React.createElement(IconButton, {tooltip: "Add File", touch: true}, 
+					React.createElement("i", {className: "fa fa-file-text-o"})
+				)
+			)
+		);
+	},
+ });
 
 },{"material-ui":2,"react/addons":93}]},{},[255,256,257,258]);
--- react/components/Widget.jsx
+++ react/components/Widget.jsx
@@ -3,27 +3,81 @@ var MUI = require('material-ui')
 var Paper = MUI.Paper;
 var FlatButton = MUI.FlatButton;
 var TextField = MUI.TextField;
+var Toolbar = MUI.Toolbar;
+var ToolbarGroup = MUI.ToolbarGroup;
+var Dialog = MUI.Dialog;
+var IconButton = MUI.IconButton
+
+var LINK_ACTIONS = [
+  { text: 'Cancel' },
+  { text: 'Submit', onClick: this.onLinkSubmit }
+];
 
 module.exports = React.createClass({
 	displayName: 'Widget',
+	getInitialState: function() {
+		return {
+			title: null,
+			descr: null
+		};
+	},
+	handleTitleChange: function(e) {
+		this.setState({
+			title: e.target.value
+		});
+	},
+	handleDescrChange: function(e) {
+		this.setState({
+			descr: e.target.value
+		});
+	},
+	showLinkDialog: function() {
+		this.refs.LinkDialog.show();
+	},
+	onLinkSubmit: function(link) {
+		console.log(link);
+	},
 	render: function() {
 		return (
 			<Paper innerClassName='flex-form' zDepth={1}>
-				<TextField hintText="Title" />
-				<FlatButton label="Add Title" primary={true} />
-				<TextField hintText="Description" multiLine={true} />
-				<FlatButton label="Add Description" primary={true} />
-				<TextField hintText="Link" />
-				<FlatButton label="Add Link" primary={true} />
-				<FlatButton primary={true}>
-					<i className="fa fa-file-image-o"></i>
-					<span className="image-button" >Upload Image</span>
-				</FlatButton>
-				<FlatButton primary={true}>
-					<i className="fa fa-file-text-o"></i>
-					<span className="image-button" >Upload File</span>
-				</FlatButton>
+				{this.renderTitle()}
+				{this.renderDescr()}
+				{this.renderButtonBar()}
+				<Dialog ref="LinkDialog" title="Dialog With Standard Actions" actions={LINK_ACTIONS}>
+				  The actions in this window are created from the json that's passed in. 
+				</Dialog>
 			</Paper>
 		);
-	}
-});
+	},
+	renderTitle: function() {
+		return (
+			<Toolbar>
+				<div className="flex-text">
+					<TextField hintText="Title" value={this.state.title} onChange={this.handleTitleChange} />
+				</div>
+			</Toolbar>
+		);
+	},
+	renderDescr: function() {
+		return (
+			<div className="flex-text">
+				<TextField hintText="Description" multiLine={true} value={this.state.descr} onChange={this.handleDescrChange} />
+			</div>
+		);
+	},
+	renderButtonBar: function() {
+		return (
+			<div className="flex-button-bar">
+				<IconButton tooltip="Add Link" touch={true}>
+					<i className="fa fa-link"></i>
+				</IconButton>
+				<IconButton tooltip="Add Image" touch={true}>
+					<i className="fa fa-file-image-o"></i>
+				</IconButton>
+				<IconButton tooltip="Add File" touch={true}>
+					<i className="fa fa-file-text-o"></i>
+				</IconButton>
+			</div>
+		);
+	},
+ });
