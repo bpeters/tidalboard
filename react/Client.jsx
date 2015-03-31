35c84ccf8778850c0bb28bd2991aa5f9b7ddf0b9
Brennen Peters <brennen.peters@civitaslearning.com>
2015-03-30 21:00:03 -0500

    Implement material ui

--- react/Client.jsx
+++ react/Client.jsx
@@ -2,6 +2,14 @@ var React = require('react/addons');
 var App = require('./App.jsx');
 var App = React.createFactory(App);
 
+var injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 var props = document.getElementById("props").innerHTML;
 props = JSON.parse(props);
 
--- react/components/Main.jsx
+++ react/components/Main.jsx
@@ -1,15 +1,48 @@
 var React = require('react/addons');
 var AppBar = require('material-ui').AppBar;
+var MenuItem = require('material-ui').MenuItem;
+var LeftNav = require('material-ui').LeftNav;
+
+var MENU_ITEMS = [
+	{ route: 'get-started', text: 'Get Started' },
+	{ route: 'css-framework', text: 'CSS Framework' },
+	{ route: 'components', text: 'Components' },
+	{ type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+	{ 
+		 type: MenuItem.Types.LINK, 
+		 payload: 'https://github.com/callemall/material-ui', 
+		 text: 'GitHub' 
+	},
+	{ 
+		 text: 'Disabled', 
+		 disabled: true 
+	},
+	{ 
+		 type: MenuItem.Types.LINK, 
+		 payload: 'https://www.google.com', 
+		 text: 'Disabled Link',
+		 disabled: true 
+	},
+];
 
 module.exports = React.createClass({
 	displayName: 'Main',
 	propTypes: {
 		params: React.PropTypes.object
 	},
+	getInitialState: function() {
+		return {
+			showLeftNav: false
+		};
+	},
+	_toggleLeftNav: function() {
+		this.refs.LeftNav.toggle();
+	},
 	render: function() {
 		return (
 			<div>
-				<AppBar title="Title" />
+				<AppBar title="Title" onMenuIconButtonTouchTap={this._toggleLeftNav} />
+				<LeftNav ref='LeftNav' docked={false} menuItems={MENU_ITEMS} />
 			</div>
 		);
 	}
